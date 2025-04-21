a66a409aca60f61ba287a844f8a09fa9ae7070b4
Zhanyl Kubatbek kyzy <kubatbekova.janyl98@gmail.com>
2025-04-21 16:44:58 +0200

    JAVA2A-17-HelloWorld-Zhanyl Create first class HelloWorld

---
+++ src/helloworld/zhanna/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld.zhanna;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Zhanyl! ";
+        String text = "Салам дүйнө";
+        System.out.println("Менин атым " + name + text + "!");
+    }
+}
