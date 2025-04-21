5cd180634961f23d058bd4362a1df3f1f9a67a9a
Yryskeldi <Yrys>
2025-04-21 13:04:56 +0600

    Java1:Create first package and new class

---
+++ src/helloworld/yryskeldi/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld.yryskeldi;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        String name = "Салам баарыныздарга!!!";
+        String text = "\nМен келдим,\nМен ";
+        Number age = 19;
+        System.out.println(name + text + age +" жаштамын.");
+    }
+}
