01662128a606fa62f077105bb4ca6d327ab43871
bony8383 <88745088+bony8383@users.noreply.github.com>
2025-04-21 16:22:18 +0200

    meerim

--- src/helloworld/Meerim/HelloWorld.java
+++ src/helloworld/Meerim/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         String s = "Salam Kyrgyzstan!";
         System.out.println( s );
+        String s1 = "Pull Request?";
+        System.out.println(s + " "+ s1);
 
     }
 }
