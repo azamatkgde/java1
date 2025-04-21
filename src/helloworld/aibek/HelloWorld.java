3efc45b023a506cbe5214177b83543eaa88ffdf8
Aibek <arstanbekov77@gmail.com>
2025-04-21 16:48:24 +0200

    create

--- src/helloworld/aibek/HelloWorld.java
+++ src/helloworld/aibek/HelloWorld.java
@@ -2,7 +2,7 @@ package helloworld.aibek;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String name =  "Aibek";
-        System.out.println(name ) ;
+        String name =  "Айбек ";
+        System.out.println(name   +  "Салам кандайсын") ;
     }
 }
