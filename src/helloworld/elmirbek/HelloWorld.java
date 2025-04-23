abbbbda684b954fff7c3320ed04a2aa9c3c0bebe
e1mirbek <elmirbekabdymanapov@gmail.com>
2025-04-23 13:44:56 +0600

    Update HelloWorld for Java2B-22

--- src/helloworld/elmirbek/HelloWorld.java
+++ src/helloworld/elmirbek/HelloWorld.java
@@ -2,6 +2,8 @@ package helloworld.elmirbek;
 
 public class HelloWorld {
     static public  void main (String[] args) {
-        System.out.println("Salam Elmirbek !!!");
+        System.out.println("Hello world");
     }
 }
+
+
