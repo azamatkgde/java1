b2e48ad0dffcd43c986e3a976c7acaebe6fd0777
Eldiiar Kurbanov <eldiiar.kurbanov96@gmail.com>
2025-05-12 08:40:57 +0200

    JAVA2A-13

--- src/helloworld/eldiiar/helloworld.java
+++ src/helloworld/eldiiar/helloworld.java
@@ -2,6 +2,6 @@ package helloworld.eldiiar;
 
 public class helloworld {
         public static void main(String[] args) {
-      System.out.println("Salam Eldiiar");
+      System.out.println("Hallo Eldiiar");
     }
 }
