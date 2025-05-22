89ea5f1f4f2fdb99cad675d9f34fc4be22c7398e
Aibek <arstanbekov77@gmail.com>
2025-05-22 10:41:24 +0200

    JAVA2A-92-OOP-Class-Object-Cat-Инкапсулияция

---
+++ src/oop/aibek/DogImpl.java
@@ -0,0 +1,11 @@
+package oop.aibek;
+
+public class DogImpl {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.setName("Лаки");
+        dog.setRace("Пудель");
+        dog.setWeight(12);
+        dog.sayGav();
+    }
+}
