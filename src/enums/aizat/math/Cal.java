8b67092df4a6966e154559683c8c6392a63a148d
arapbaevaaizat <aizaarapbaeva@gmail.com>
2025-07-11 23:20:03 +0900

    Delete src/enums/aizat/math/Cal.java

--- src/enums/aizat/math/Cal.java
+++
@@ -1,8 +0,0 @@
-package enums.aizat.math;
-
-public class Cal {
-    public static void main(String[] args){
-        Operation oper = Operation.Slojeniya;
-        System.out.println(oper.act (10, 15));
-    }
-}
