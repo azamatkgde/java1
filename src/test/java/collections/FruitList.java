1983c8bce94477767473f5cf28a9b357ec9e4295
Bekbolot97 <bekbolot.namatov@gmail.com>
2025-07-18 18:42:41 +0200

    Extentions Try Catch

--- src/test/java/collections/FruitList.java
+++ src/test/java/collections/FruitList.java
@@ -1,10 +1,13 @@
 package collections;
 
+import abstractclass.aizat.SouthKorea;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FruitList {
     public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
         ArrayList<String> fruits = new ArrayList<>();
 
         fruits.add("Алмурут");
